a0e7a00652930d596df513bd66f989caa2702418
tretiak-andrii <andrii.tretiak@sombrainc.com>
2022-10-28 18:03:25 +0300

    fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('middle-promotion');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('middle-promotion app is running!');
-  });
 });
