7ed1cf59e46db3dc2454b21129ada50b6e17cb37
tretiak-andrii <andrii.tretiak@sombrainc.com>
2022-10-28 17:55:27 +0300

    updated webpack fix 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SadComponent } from './sad/sad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SadComponent
   ],
   imports: [
     BrowserModule
