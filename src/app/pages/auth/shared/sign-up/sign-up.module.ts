ff284c17b4c96a00325ec14c96158eb3659e05d9
tretiak-andrii <andrii.tretiak@sombrainc.com>
2023-07-12 11:44:07 +0300

    fix js linter

--- src/app/pages/auth/shared/sign-up/sign-up.module.ts
+++ src/app/pages/auth/shared/sign-up/sign-up.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from "./sign-up.component";
 import { SignUpRoutingModule } from "./signUp-routing.module";
-import {BasicLoginLayoutModule} from "../../../../layout/basic-login-layout/basic-login-layout.module";
+import { BasicLoginLayoutModule } from "../../../../layout/basic-login-layout";
 
 
 
